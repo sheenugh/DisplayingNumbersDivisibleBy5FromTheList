52b281771e869fa3c8d0c62465660dcc7122144e
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 10:13:34 +0800

    changes because di nagbabasa ng instructions: name ng file, mga var name sa program + changes sa draft file din ay inayos ko

--- numbers_divisible_by_5_from_the_list.py
+++ numbers_divisible_by_5_from_the_list.py
@@ -1,7 +1,7 @@
 
 
 # ----- FUNCTIONS -----
-def is_even(given):
+def divisibility_by_5(given):
     for i in range(len(given)):
         if given[i] % 5 == 0:
             print(given[i])
@@ -10,12 +10,12 @@ def is_even(given):
 
 # ----- PSEUDO CODE -----
 # - Actual Code
-given_list_or_set = (10,20,33,46,55)
+given_list_or_set_of_numbers = (10,20,33,46,55)
 
-print("Given list/set is " + str(given_list_or_set) + " . Find the numbers whose divisible by 5.")
+print("The given list/set of number is " + str(given_list_or_set_of_numbers) + " . Find the numbers whose divisible by 5.")
 print("The numbers who are divisible by 5 are: ")
 
-is_even(given_list_or_set)
+divisibility_by_5(given_list_or_set_of_numbers)
 
 
 
--- trying_the_code_on_my_own.py
+++ trying_the_code_on_my_own.py
@@ -2,15 +2,15 @@
 # draft ko lang ito kasi mabagal utak ko  mag isip ng algorithm. if nagwork naman nililipat ko sa main .py or file ko
 
 
-def is_even(given):
+def divisibility_by_5(given):
     for i in range(len(given)):
         if given[i] % 5 == 0:
             print(given[i])
         else:
             False
 
-given = (10,20,33,46,55)
-is_even(given)
+given_list_or_set_of_numbers = (10,20,33,46,55)
+divisibility_by_5(given_list_or_set_of_numbers)
 # gumana na!
 
 # Thank You
