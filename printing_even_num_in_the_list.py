b17164486d2d9a98403160061dae73f0c5929be6
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 09:58:37 +0800

    adding the conclusion statement for result. Done na din ako

--- printing_even_num_in_the_list.py
+++ printing_even_num_in_the_list.py
@@ -13,7 +13,7 @@ def is_even(given):
 given_list_or_set = (10,20,33,46,55)
 
 print("Given list/set is " + str(given_list_or_set) + " . Find the numbers whose divisible by 5.")
-
+print("The numbers who are divisible by 5 are: ")
 
 is_even(given_list_or_set)
 
