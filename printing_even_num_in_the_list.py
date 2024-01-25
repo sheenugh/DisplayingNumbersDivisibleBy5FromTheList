c64097988410392e902981da9f6b22fd487d32d9
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 09:57:28 +0800

    adding statement b4 the result

--- printing_even_num_in_the_list.py
+++ printing_even_num_in_the_list.py
@@ -3,17 +3,19 @@
 # ----- FUNCTIONS -----
 def is_even(given):
     for i in range(len(given)):
-        if given[i] % 2 == 0:
+        if given[i] % 5 == 0:
             print(given[i])
         else:
-            print("invalid")
-
-
+            False
 
 # ----- PSEUDO CODE -----
 # - Actual Code
-given = (10,20,33,46,55)
-is_even(given)
+given_list_or_set = (10,20,33,46,55)
+
+print("Given list/set is " + str(given_list_or_set) + " . Find the numbers whose divisible by 5.")
+
+
+is_even(given_list_or_set)
 
 
 
--- trying_the_code_on_my_own.py
+++ trying_the_code_on_my_own.py
@@ -1,9 +1,16 @@
+#DRAFT
+# draft ko lang ito kasi mabagal utak ko  mag isip ng algorithm. if nagwork naman nililipat ko sa main .py or file ko
+
+
 def is_even(given):
     for i in range(len(given)):
-        if given[i] % 2 == 0:
+        if given[i] % 5 == 0:
             print(given[i])
         else:
-            print("invalid")
+            False
 
 given = (10,20,33,46,55)
 is_even(given)
+# gumana na!
+
+# Thank You
