af65786ac8bfabe1f1ae754ac43dbe3d6eb7a923
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 14:15:44 +0800

    changes lang sa paggamit ng words and such characters sa comments + adding comment

--- numbers_divisible_by_5_from_the_list.py
+++ numbers_divisible_by_5_from_the_list.py
@@ -1,6 +1,7 @@
 
 
 # ----- FUNCTIONS -----
+# - This section is for the def function of divisibility by 5
 def divisibility_by_5(given):
     for i in range(len(given)):
         if given[i] % 5 == 0:
@@ -8,15 +9,17 @@ def divisibility_by_5(given):
         else:
             False
 
-# ----- PSEUDO CODE -----
-# - Actual Code
+
+#>>>>>>>>>> PSEUDO CODE <<<<<<<<<<
+#----- ACTUAL CODES -----
+# - Given set of numbers
 given_list_or_set_of_numbers = (10,20,33,46,55)
 
+# - Printing the result
 print("The given list/set of number is " + str(given_list_or_set_of_numbers) + " . Find the numbers whose divisible by 5.")
 print("The numbers who are divisible by 5 are: ")
 
+# - Caller in def function
 divisibility_by_5(given_list_or_set_of_numbers)
 
 
-
-# - Result
